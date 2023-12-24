25ac474691235ef2b8b8019dfe0ac8b5a1d5bd28
Abdulrahman Khalid <a.khalid@procrew.pro>
2023-12-24 19:13:38 +0200

    feat: add a demo for rabbitmq pub/sub

---
+++ DotnetRabbitMQDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new()
+{
+    Uri = new("amqp://guest:guest@localhost:5672"),
+    ClientProvidedName = "RabbitMQ Sender App"
+};
+
+IConnection connection = factory.CreateConnection();
+
+IModel channel = connection.CreateModel();
+
+string exchangeName = "demo-exchange";
+string routingKey = "demo-routing-key";
+string queueName = "demo-queue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, false, false, false, null);
+channel.QueueBind(queueName, exchangeName, routingKey, null);
+
+for (int i = 0; i <60; i++)
+{
+    Console.WriteLine($"Sending Message #{i}");
+    byte[] messageBodyBytes = Encoding.UTF8.GetBytes($"Message ${i}");
+    channel.BasicPublish(exchangeName, routingKey, null, messageBodyBytes);
+
+    Thread.Sleep(1000);
+}
+
+channel.Close();
+connection.Close();
+
